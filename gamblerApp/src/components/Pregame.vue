21bc9979c7b5b2e6e8aff07af8501f086ab522d4
Brian Nduhiu <briannduhiu@gmail.com>
2022-11-02 10:46:27 +0300

    Gambler App design update

--- gamblerApp/src/components/Pregame.vue
+++ gamblerApp/src/components/Pregame.vue
@@ -1,5 +1,6 @@
-<template>
-    <p class="numPickText">What are your 3 lucky numbers? (0-9)</p>
+<template >
+    <div class="pregame">
+        <p class="numPickText">What are your 3 lucky numbers? (0-9)</p>
     <form class="pregameForm">
             <input type="number" name="lucky1" id="lucky1" min="0" max="9" class="pregameInput" required>
             <input type="number" name="lucky2" id="lucky2" min="0" max="9" class="pregameInput" required>
@@ -7,4 +8,6 @@
             <button type="submit" class="pregameInput">Play</button>
         </form>
         <p class="instructions">Click <span>Play</span> to reveal the numbers</p>
+    </div>
+    
 </template>
