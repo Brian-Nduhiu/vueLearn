36b92ff1d4be2c1be85991835db248740d83f4a5
Brian Nduhiu <briannduhiu@gmail.com>
2022-11-07 16:13:51 +0300

    Component Breakdown

--- vegasPinia/src/App.vue
+++ vegasPinia/src/App.vue
@@ -131,3 +131,108 @@ export default {
     </h1>
   </div>
 </template>
+
+
+
+<!-- 
+
+  Component Breakdown
+
+  App
+
+    userInput
+
+<p class="numPickText">What are your 3 lucky numbers? (0-9)</p>
+    <form>
+      <input
+        v-model="userInput[0]"
+        type="number"
+        name="lucky1"
+        id="lucky1"
+        class="input"
+        min="0"
+        max="9"
+        required
+      />
+      <input
+        v-model="userInput[1]"
+        type="number"
+        name="lucky2"
+        id="lucky2"
+        class="input"
+        min="0"
+        max="9"
+        required
+      />
+      <input
+        v-model="userInput[2]"
+        type="number"
+        name="lucky3"
+        id="lucky3"
+        class="input"
+        min="0"
+        max="9"
+        required
+      />
+      <button
+        type="button"
+        @click="
+          logCalls();
+          generator();
+          resultsChecker();
+        "
+        class="input"
+      >
+        Play
+      </button>
+    </form>
+
+    <p class="instructions">Click <span>Play</span> to reveal the numbers</p>
+
+
+    Generator
+
+
+<div class="buttons">
+      <button
+        :class="{
+          win: winCheck[0] == true,
+          lose: winCheck[0] == false,
+        }"
+        class="buttonResults"
+      >
+        {{ generatedNums[0] }}
+      </button>
+      <button
+        :class="{
+          win: winCheck[1] == true,
+          lose: winCheck[1] == false,
+        }"
+        class="buttonResults"
+      >
+        {{ generatedNums[1] }}
+      </button>
+      <button
+        :class="{
+          win: winCheck[2] == true,
+          lose: winCheck[2] == false,
+        }"
+        class="buttonResults"
+      >
+        {{ generatedNums[2] }}
+      </button>
+    </div>
+
+
+    Results
+
+
+<h1 class="results" v-if="hasWon">{{ resultsMessage[0] }}</h1>
+    <h1 class="results" v-else-if="hasWon == false && hasWon != null">
+      {{ resultsMessage[1] }}
+    </h1>
+
+
+
+
+ -->
