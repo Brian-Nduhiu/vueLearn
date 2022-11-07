734bc3afac8b25a5f50090fd05bcd1a53f195a38
Brian Nduhiu <briannduhiu@gmail.com>
2022-11-07 08:55:07 +0300

    vegasSFC build

--- vegasSFC/src/App.vue
+++ vegasSFC/src/App.vue
@@ -75,9 +75,18 @@ export default{
 
 <div class="buttons">
 
-    <button class="buttonResults ">{{generatedNums[0]}}</button>
-    <button class="buttonResults ">{{generatedNums[1]}}</button>
-    <button class="buttonResults ">{{generatedNums[2]}}</button>
+    <button :class="{
+      'win': winCheck[0] == true,
+      'lose': winCheck[0] == false
+    }" class="buttonResults">{{generatedNums[0]}}</button>
+    <button :class="{
+      'win': winCheck[1] == true,
+      'lose': winCheck[1] == false
+    }" class="buttonResults">{{generatedNums[1]}}</button>
+    <button :class="{
+      'win': winCheck[2] == true,
+      'lose': winCheck[2] == false
+    }" class="buttonResults">{{generatedNums[2]}}</button>
 
 </div>
 
