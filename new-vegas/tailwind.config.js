90ec4d04d72b063e710ee98686457a2363ffcc9b
Brian Nduhiu <briannduhiu@gmail.com>
2022-11-09 17:01:38 +0300

    Dark Mode

--- new-vegas/tailwind.config.js
+++ new-vegas/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
